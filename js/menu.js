20407189d2e07f65a5f44bbb397d3cee59073b82
genucell <genucell@genucell.co.kr>
2019-05-22 21:11:47 +0900

    아 집가고싶다

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,22 @@ function closeDiv(){
     var closeDiv = document.getElementById("myDiv");
   closeDiv.style.transform = "scale(0)"; 
 }
+
+
+// show/hide footer content - change tab text
+$('.button-wrapper').click(function(){
+	$('.footer-wrapper').toggleClass('open');
+	
+	if ( $('.footer-wrapper').hasClass('open') ) {
+		$('.button-wrapper button').text("Close");
+	} else {
+		$('.button-wrapper button').text("Open");
+	}
+});
+
+$('.footer-wrapper').css("border","1px solid #000");
+$('.button-wrapper button').css("background-color","#000");
+
+$(".footer-wrapper").hover(function(){
+  $(this).css("background-color", "black");
+});
